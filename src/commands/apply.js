cf7dc0ff7eb4596eddf89b06f93f595aaae8a808
Anna Banana <annabanana2577@outlook.com>
2023-03-13 11:47:24 -0400

    added /apply command

---
+++ src/commands/apply.js
@@ -0,0 +1,31 @@
+import { SlashCommandBuilder } from "discord.js";
+
+export const data = new SlashCommandBuilder()
+  .setName("apply")
+  .setDescription("apply to join a clan")
+  .addStringOption((option) =>
+    option
+      .setName("clanname")
+      .setDescription("the name of the clan you wish to apply to")
+      .setRequired(true)
+  );
+
+export async function execute({ interaction, db }) {
+  await interaction.deferReply();
+  const clanName = interaction.options.getString("clanname");
+  const clan = await db.clans.get(clanName);
+  if (!clan) return interaction.editReply(`Clan ${clanName} doesn't exist.`);
+  if (
+    clan.leader == interaction.user.id ||
+    clan.players.includes(interaction.user.id) ||
+    clan.applicants.includes(interaction.user.id)
+  )
+    return interaction.editReply(
+      `You are already in, or have already applied, to ${clanName} clan`
+    );
+  clan.applicants.push(interaction.user.id);
+  await db.clans.set(clanName, clan);
+  interaction.editReply(
+    `<@${interaction.user.id}> has applied to ${clanName} clan!`
+  );
+}
